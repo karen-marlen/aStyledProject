3f296953ac7f613fad065e117e180492b5f189d2
karenzhec@gmail.com <karenzhec@gmail.com>
2021-06-21 20:53:35 -0300

    headerSection

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,15 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href='/'>
-        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20' }}>
           <DiCssdeck size='3rem' />
-          <span>Portfolio</span>
+          <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
@@ -32,6 +32,17 @@ const Header = () =>  (
           </Link>
         </li>
     </Div2>
+    <Div3>
+      <SocialIcons href='https://www.github.com'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com'>
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+      <SocialInstagram href='https://www.instagram.com'>
+        <AiFillGithub size='3rem' />
+      </SocialInstagram>
+    </Div3>
   </Container>
 );
 
