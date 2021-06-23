4972b2fab03c5986ad18b6ce7942e1fdce8a5ceb
karenzhec@gmail.com <karenzhec@gmail.com>
2021-06-23 16:13:35 -0300

    heroSection

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopading>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <bt />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of JavaScript is to help aspiring and established etc etc
+      </SectionText>
+      <Button onClick={() => window.location = 'http://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
